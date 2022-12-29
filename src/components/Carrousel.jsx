609eee55632c96342608d2139a6f526b58d82d10
Clet Nathan <nanouclet@gmail.com>
2022-12-29 23:16:30 +0100

    finalisation slider

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -9,12 +9,11 @@ export default function Carrousel ({slides}){
     const nextSlide = () => {
         setCurrent(current === length - 1 ? 0 : current + 1);
     }
-	
-	
+
     return(
         <div className="carrousel">
-            <i className="fa-sharp fa-solid fa-chevron-left arrow-left" onClick={previousSlide}></i>
-            <i className="fa-sharp fa-solid fa-chevron-right arrow-right" onClick={nextSlide}></i>
+            {length > 1 && <i className="fa-sharp fa-solid fa-chevron-left arrow-left" onClick={previousSlide}></i>}
+            {length > 1 &&<i className="fa-sharp fa-solid fa-chevron-right arrow-right" onClick={nextSlide}></i>}
             <img src={slides[current]} alt="Appartement à louer" />
             <div className="carrousel_number">{current+1}/{length}</div>
         </div>
